e8ba2d9834ecd2f263a3c67777f55415f40a578c
qiushi12 <95695525+qiushi12@users.noreply.github.com>
2023-02-16 14:00:16 +0800

    Delete application directory

--- application/scripts/AGV_APP/templates/APP/js/vue.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-    el: "#app",
-    data() {
-        return {
-            username: ""
-        }
-    }
-});
--- application/scripts/wsgi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from AGV_APP import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
